2107551f92524c288d898212ce0be4b037fcad8e
Arthur236 <arthur.thungu@gmail.com>
2017-11-13 12:07:39 +0300

    [Chore #151995973] Add Footer component

---
+++ src/components/common/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <div className="row page-footer wow fadeInDown">
+                <p className="copyright">
+                    ©2017 Shopping List Application. A project by <a href="#">Arthur Thungu</a>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
