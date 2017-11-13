2b14545293cce4ac6f2f4a7f722266c2b2cecc93
Arthur236 <arthur.thungu@gmail.com>
2017-11-13 12:07:23 +0300

    [Chore #151995973] Add Background slider component

---
+++ src/components/common/BackgroundSlider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class BackgroundSlider extends React.Component {
+    render() {
+        return(
+            <div>
+                <div className="slider fullscreen">
+                    <ul className="slides">
+                        <li><img src={require('../../styles/img/img1.jpg')} alt="Image 1" /></li>
+                        <li><img src={require('../../styles/img/img2.jpg')} alt="Image 2" /></li>
+                        <li><img src={require('../../styles/img/img3.jpg')} alt="Image 3" /></li>
+                        <li><img src={require('../../styles/img/img4.jpg')} alt="Image 4" /></li>
+                        <li><img src={require('../../styles/img/img5.jpeg')} alt="Image 5" /></li>
+                        <li><img src={require('../../styles/img/img6.jpeg')} alt="Image 6" /></li>
+                    </ul>
+                </div>
+
+                <div className="overlay"></div>
+            </div>
+        );
+    }
+}
+
+export default BackgroundSlider;
